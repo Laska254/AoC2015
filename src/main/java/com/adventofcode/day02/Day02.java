08eca607df75504bc64f3d35dc2b3665b67d55fa
Laska254 <w.turczak254@gmail.com>
2026-03-30 13:19:00 +0200

    Extracted calculations to helper methods Added tests

--- src/main/java/com/adventofcode/day02/Day02.java
+++ src/main/java/com/adventofcode/day02/Day02.java
@@ -4,26 +4,31 @@ import java.util.List;
 
 public class Day02 {
 
-    public int getTotalSquareFeet(List<List<Integer>> listOfDimensions) {
-        int total = 0;
-        for (List<Integer> dimensions : listOfDimensions) {
-            int area1 = 2 * dimensions.get(0) * dimensions.get(2);
-            int area2 = 2 * dimensions.get(2) * dimensions.get(1);
-            int area3 = 2 * dimensions.get(1) * dimensions.get(0);
-            int slack = Math.min(Math.min(area1, area2), area3) / 2;
-            total += area1 + area2 + area3 + slack;
-        }
-        return total;
+    public int getTotalSquareFeet(final List<List<Integer>> listOfDimensions) {
+        return listOfDimensions.stream()
+                .mapToInt(this::calculateSquareFeet)
+                .sum();
     }
 
-    public int getTotalLength(List<List<Integer>> listOfDimensions){
-        int total = 0;
-        for (List<Integer> dimensions : listOfDimensions){
-            List<Integer> list = dimensions.stream().sorted().toList();
-            int length = (2 * list.get(0)) + (2 * list.get(1));
-            int bow = dimensions.get(0) * dimensions.get(1) * dimensions.get(2);
-            total += length + bow;
-        }
-        return total;
+    public int getTotalLength(final List<List<Integer>> listOfDimensions) {
+        return listOfDimensions.stream()
+                .mapToInt(this::calculateLength)
+                .sum();
     }
+
+    public int calculateSquareFeet(final List<Integer> dimensions) {
+        final var area1 = 2 * dimensions.get(0) * dimensions.get(2);
+        final var area2 = 2 * dimensions.get(2) * dimensions.get(1);
+        final var area3 = 2 * dimensions.get(1) * dimensions.get(0);
+        final var slack = Math.min(Math.min(area1, area2), area3) / 2;
+        return area1 + area2 + area3 + slack;
+    }
+
+    public int calculateLength(final List<Integer> dimensions) {
+        final var sorted = dimensions.stream().sorted().toList();
+        final var ribbon = (2 * sorted.get(0)) + (2 * sorted.get(1));
+        final var bow = dimensions.get(0) * dimensions.get(1) * dimensions.get(2);
+        return ribbon + bow;
+    }
+
 }
---
+++ src/test/java/com/adventofcode/day02/Day02Test.java
@@ -0,0 +1,46 @@
+package com.adventofcode.day02;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Day02Test {
+
+    private Day02 day02;
+
+    @BeforeEach
+    void setUp() {
+        day02 = new Day02();
+    }
+
+    /* A present with dimensions `2x3x4` requires `2*6 + 2*12 + 2*8 = 52`
+     square feet of wrapping paper plus 6 square feet of slack, for a total of 58 square feet.
+     A present with dimensions `1x1x10` requires `2*1 + 2*10 + 2*10 = 42`
+     square feet of wrapping paper plus 1 square foot of slack, for a total of 43 square feet.*/
+    @ParameterizedTest
+    @CsvSource({
+            "2,3,4,58",
+            "1,1,10,43"
+    })
+    void calculateSquareFeet_shouldCorrectlyCalculate(int length, int width, int height, int expected) {
+        assertEquals(expected, day02.calculateSquareFeet(List.of(length, width, height)));
+    }
+
+    /* A present with dimensions `2x3x4` requires `2+2+3+3 = 10` feet of ribbon
+     to wrap the present plus `2*3*4 = 24` feet of ribbon for the bow, for a total of 34 feet.
+     A present with dimensions `1x1x10` requires `1+1+1+1 = 4` feet of ribbon
+     to wrap the present plus `1*1*10 = 10` feet of ribbon for the bow, for a total of 14 feet. */
+    @ParameterizedTest()
+    @CsvSource({
+            "2, 3, 4,  34",
+            "1, 1, 10, 14"
+    })
+    void calculateLength_shouldCorrectlyCalculate(int length, int width, int height, int expected) {
+        assertEquals(expected, day02.calculateSquareFeet(List.of(length, width, height)));
+    }
+
+}
