c429ac8f5700065187a94558165409a23f15711b
Laska254 <w.turczak254@gmail.com>
2026-03-30 23:46:47 +0200

    moved try-catch to constructor used HexFormat

--- src/main/java/com/adventofcode/day04/Day04.java
+++ src/main/java/com/adventofcode/day04/Day04.java
@@ -2,21 +2,23 @@ package com.adventofcode.day04;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.HexFormat;
 
 public class Day04 {
 
-    private String calculateMD5(final String input) {
+    private final MessageDigest md5;
+
+    public Day04() {
         try {
-            MessageDigest md5 = MessageDigest.getInstance("MD5");
-            byte[] bytes = md5.digest(input.getBytes());
-            StringBuilder sb = new StringBuilder();
-            for (byte b : bytes) {
-                sb.append(String.format("%02x", b));
-            }
-            return sb.toString();
-        } catch (NoSuchAlgorithmException exception) {
-            throw new RuntimeException("Algorithm not found.", exception);
+            this.md5 = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("MD5 algorithm not found.", e);
         }
+    }
+
+    private String calculateMD5(final String input) {
+        md5.reset();
+        return HexFormat.of().formatHex(md5.digest(input.getBytes()));
     }
 
     public int findLowestPossibleNumber(final String secretKey, final String requirement) {
