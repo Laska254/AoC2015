d593d6c21883542691d7bdb68b004c7b0f2e3b44
Laska254 <w.turczak254@gmail.com>
2026-03-31 13:35:34 +0200

    final, vars and fancy if else

--- src/main/java/com/adventofcode/day06/Instruction.java
+++ src/main/java/com/adventofcode/day06/Instruction.java
@@ -4,8 +4,8 @@ import com.adventofcode.day03.Coordinates;
 
 public record Instruction(String command, Coordinates start, Coordinates end) {
 
-    public static Instruction fromString(String instruction) {
-        String command = parseCommand(instruction);
+    public static Instruction fromString(final String instruction) {
+        final var command = parseCommand(instruction);
         if (instruction.split(" ").length == 5) {
             return new Instruction(command,
                     parseCoords(instruction, 2),
@@ -17,18 +17,17 @@ public record Instruction(String command, Coordinates start, Coordinates end) {
         }
     }
 
-    private static String parseCommand(String instruction) {
-        String[] split = instruction.split(" ");
-        if (instruction.contains("turn")) {
-            return split[0] + " " + split[1];
-        } else {
-            return split[0];
-        }
+    private static String parseCommand(final String instruction) {
+        final var split = instruction.split(" ");
+        return instruction.contains("turn")
+                ? split[0] + " " + split[1]
+                : split[0];
     }
 
-    private static Coordinates parseCoords(String instruction, int index) {
-        String[] split = instruction.split(" ");
-        String[] coords = split[index].split(",");
+    private static Coordinates parseCoords(final String instruction, final int index) {
+        final var split = instruction.split(" ");
+        final var coords = split[index].split(",");
         return new Coordinates(Integer.parseInt(coords[0]), Integer.parseInt(coords[1]));
     }
+
 }
