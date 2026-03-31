c648128d55be285faf624017a663c595e9ca8817
Laska254 <w.turczak254@gmail.com>
2026-03-31 14:44:54 +0200

    used Pattern instead

--- src/main/java/com/adventofcode/day06/Instruction.java
+++ src/main/java/com/adventofcode/day06/Instruction.java
@@ -2,32 +2,24 @@ package com.adventofcode.day06;
 
 import com.adventofcode.day03.Coordinates;
 
-public record Instruction(String command, Coordinates start, Coordinates end) {
+import java.util.regex.Pattern;
+
+public record Instruction(Command command, Coordinates start, Coordinates end) {
+
+    private static final Pattern PATTERN = Pattern.compile(
+            "(turn on|turn off|toggle) (\\d+),(\\d+) through (\\d+),(\\d+)"
+    );
 
     public static Instruction fromString(final String instruction) {
-        final var command = parseCommand(instruction);
-        if (instruction.split(" ").length == 5) {
-            return new Instruction(command,
-                    parseCoords(instruction, 2),
-                    parseCoords(instruction, 4));
-        } else {
-            return new Instruction(command,
-                    parseCoords(instruction, 1),
-                    parseCoords(instruction, 3));
+        final var matcher = PATTERN.matcher(instruction);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("Invalid instruction: " + instruction);
         }
-    }
-
-    private static String parseCommand(final String instruction) {
-        final var split = instruction.split(" ");
-        return instruction.contains("turn")
-                ? split[0] + " " + split[1]
-                : split[0];
-    }
-
-    private static Coordinates parseCoords(final String instruction, final int index) {
-        final var split = instruction.split(" ");
-        final var coords = split[index].split(",");
-        return new Coordinates(Integer.parseInt(coords[0]), Integer.parseInt(coords[1]));
+        return new Instruction(
+                Command.fromString(matcher.group(1)),
+                new Coordinates(Integer.parseInt(matcher.group(2)), Integer.parseInt(matcher.group(3))),
+                new Coordinates(Integer.parseInt(matcher.group(4)), Integer.parseInt(matcher.group(5)))
+        );
     }
 
 }
