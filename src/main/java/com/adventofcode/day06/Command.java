dbb2692446a61fc8a0546676ad011459c8ec956e
Laska254 <w.turczak254@gmail.com>
2026-03-31 20:13:47 +0200

    non-abstract apply()

--- src/main/java/com/adventofcode/day06/Command.java
+++ src/main/java/com/adventofcode/day06/Command.java
@@ -1,26 +1,17 @@
 package com.adventofcode.day06;
 
 public enum Command {
-    TURN_ON {
-        @Override
-        public Light apply(final Light light) {
-            return new Light(true, light.brightness() + 1);
-        }
-    },
-    TURN_OFF {
-        @Override
-        public Light apply(final Light light) {
-            return new Light(false, Math.max(light.brightness() - 1, 0));
-        }
-    },
-    TOGGLE {
-        @Override
-        public Light apply(final Light light) {
-            return new Light(!light.state(), light.brightness() + 2);
-        }
-    };
+    TURN_ON,
+    TURN_OFF,
+    TOGGLE;
 
-    public abstract Light apply(final Light light);
+    public Light apply(final Light light) {
+        return switch (this) {
+            case TURN_ON -> new Light(true, light.brightness() + 1);
+            case TURN_OFF -> new Light(false, Math.max(light.brightness() - 1, 0));
+            case TOGGLE -> new Light(!light.state(), light.brightness() + 2);
+        };
+    }
 
     public static Command fromString(String command) {
         return switch (command) {
