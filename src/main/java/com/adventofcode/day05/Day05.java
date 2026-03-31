255f1abf1c0c158304cc57ded9bb435ab7e78da5
Laska254 <w.turczak254@gmail.com>
2026-03-31 13:20:39 +0200

    patternsFor instead of rules Map

--- src/main/java/com/adventofcode/day05/Day05.java
+++ src/main/java/com/adventofcode/day05/Day05.java
@@ -1,20 +1,9 @@
 package com.adventofcode.day05;
 
 import java.util.List;
-import java.util.Map;
 
 
 public class Day05 {
-
-    private static final Map<CheckingMethod, List<NicePattern>> RULES = Map.ofEntries(
-            Map.entry(CheckingMethod.STANDARD, List.of(
-                    NicePattern.THREE_VOWELS,
-                    NicePattern.ONE_LETTER_TWICE_IN_ROW,
-                    NicePattern.NO_BANNED_WORDS)),
-            Map.entry(CheckingMethod.BETTER, List.of(
-                    NicePattern.PAIR_LETTERS_TWICE_WITHOUT_OVERLAPPING,
-                    NicePattern.A_B_A))
-    );
 
     public int howManyStringsAreNice(final List<String> list, final CheckingMethod method) {
         return (int) list.stream()
@@ -23,7 +12,7 @@ public class Day05 {
     }
 
     public boolean isNice(final String word, final CheckingMethod method) {
-        return RULES.getOrDefault(method, List.of())
+        return NicePattern.patternsFor(method)
                 .stream()
                 .allMatch(pattern -> pattern.matches(word));
     }
