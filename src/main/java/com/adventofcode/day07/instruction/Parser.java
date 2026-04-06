8b914c9fdefefe872592195f60f3ab243a7d8535
Laska254 <w.turczak254@gmail.com>
2026-04-06 11:05:36 +0200

    Rebuild Parser

--- src/main/java/com/adventofcode/day07/instruction/Parser.java
+++ src/main/java/com/adventofcode/day07/instruction/Parser.java
@@ -3,36 +3,20 @@ package com.adventofcode.day07.instruction;
 public class Parser {
 
     public Instruction parse(final String line) {
-        final String[] split = line.split(" ");
-        if (split.length == 3) {
-            return parseAssign(split);
-        }
-        if (split.length == 4 && "NOT".equals(split[0])) {
-            return parseNot(split);
-        }
-        if (split.length == 5 && ("AND".equals(split[1]) || "OR".equals(split[1]))) {
-            return parseAndOr(split);
-        }
-        if (split[1].contains("SHIFT")) {
-            return parseShift(split);
-        }
-        throw new IllegalArgumentException("Unknown instruction: " + line);
-    }
-
-    private Instruction parseAssign(final String[] split) {
-        return new Assign(split[0], split[2]);
-    }
-
-    private static Instruction parseNot(final String[] split) {
-        return new Not(split[1], split[3]);
-    }
-
-    private static Instruction parseAndOr(final String[] split) {
-        return new AndOr(split[0], split[1], split[2], split[4]);
-    }
-
-    private static Instruction parseShift(final String[] split) {
-        return new Shift(split[0], split[1], Integer.parseInt(split[2]), split[4]);
+        final var split = line.split(" -> ");
+        final var operation = split[0];
+        final var target = split[1];
+        final var tokens = operation.split(" ");
+        return switch (tokens.length) {
+            case 1 -> new Assign(tokens[0], target);
+            case 2 -> new Not(tokens[1], target);
+            case 3 -> switch (tokens[1]) {
+                case "AND", "OR" -> new AndOr(tokens[0], tokens[1], tokens[2], target);
+                case "LSHIFT", "RSHIFT" -> new Shift(tokens[0], tokens[1], Integer.parseInt(tokens[2]), target);
+                default -> throw new IllegalArgumentException("Unknown operator: " + tokens[1]);
+            };
+            default -> throw new IllegalArgumentException("Unknown instruction: " + line);
+        };
     }
 
 }
