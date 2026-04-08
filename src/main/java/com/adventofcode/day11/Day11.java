e24e1604f2941309ab78973e2ceda6d3171af31c
Laska254 <w.turczak254@gmail.com>
2026-04-08 22:10:47 +0200

    final & vars & format fix

--- src/main/java/com/adventofcode/day11/Day11.java
+++ src/main/java/com/adventofcode/day11/Day11.java
@@ -7,23 +7,25 @@ public class Day11 {
     private static final List<Character> BANNED_LETTERS = List.of('i', 'o', 'l');
     private static final String TWO_DIFFERENT_NON_OVERLAPPING_PAIRS = "(?:([a-z])\\1.*([a-z])\\2|[a-z]*([a-z])\\3[a-z]*){2,}";
 
-    public String generateNewPassword(String oldPassword) {
-        String newPassword = oldPassword;
+    public String generateNewPassword(final String oldPassword) {
+        var newPassword = oldPassword;
         do {
             newPassword = incrementPassword(newPassword);
         } while (!checkIfValid(newPassword));
         return newPassword;
     }
 
-    public boolean checkIfValid(String password) {
-        return increasingStraightOfThreeLetters(password) && !checkIfContainsBannedLetters(password) && password.matches(TWO_DIFFERENT_NON_OVERLAPPING_PAIRS);
+    public boolean checkIfValid(final String password) {
+        return increasingStraightOfThreeLetters(password)
+                && !checkIfContainsBannedLetters(password)
+                && password.matches(TWO_DIFFERENT_NON_OVERLAPPING_PAIRS);
     }
 
-    private boolean increasingStraightOfThreeLetters(String password) {
+    private boolean increasingStraightOfThreeLetters(final String password) {
         for (int i = 0; i < password.length() - 2; i++) {
-            char first = password.charAt(i);
-            char second = (char) (first + 1);
-            char third = (char) (first + 2);
+            final var first = password.charAt(i);
+            final var second = (char) (first + 1);
+            final var third = (char) (first + 2);
             if (password.charAt(i + 1) == second && password.charAt(i + 2) == third) {
                 return true;
             }
@@ -31,13 +33,13 @@ public class Day11 {
         return false;
     }
 
-    private boolean checkIfContainsBannedLetters(String password) {
+    private boolean checkIfContainsBannedLetters(final String password) {
         return password.chars()
                 .anyMatch(letter -> BANNED_LETTERS.contains((char) letter));
     }
 
-    private String incrementPassword(String password) {
-        char[] chars = password.toCharArray();
+    private String incrementPassword(final String password) {
+        final var chars = password.toCharArray();
         for (int i = chars.length - 1; i >= 0; i--) {
             if (chars[i] == 'z') {
                 chars[i] = 'a';
